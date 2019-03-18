574ed48fa6b1ff0771458a685d2ed9de3981808e
c1ear1k c1ear1k <5584038@inbox.ru>
2019-03-18 17:27:22 +0300

    HotFix IFactory_FullDataTest "прогулочная площадка детского сада" 3.5.1.0, 12.0.1 => 12.0.1

--- Classifier.Tests/IFactoryTests.cs
+++ Classifier.Tests/IFactoryTests.cs
@@ -14,6 +14,8 @@ namespace Classifier.Tests
             37181, "3.1.1", false, false, false, "3.6.1, 5.1.1", 100, 1000)]
         [TestCase("автомобильный транспорт (7.2) (земельные участки, предназначенные для разработки полезных ископаемых, размещения железнодорожных путей, ав",
             76, "6.7.0, 7.1.2", false, false, false, "7.2.1", 500, 5000)]
+        [TestCase("эксплуатации прогулочной площадки детского сада",
+            1416, "", false, false, false, "12.0.1", 400, 4001)]
         public void IFactory_FullDataTest(string _vri_doc, int _area, string _btiVri, bool _lo, bool _mid, bool _hi, string vri_list, int type, int kind)
         {
             IInputData data = new InputData(_vri_doc, _area, _btiVri, _lo, _mid, _hi);
